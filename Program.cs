95bda0e87e960a929b7037a127b58a5e8fcd8743
Гафиев Владислав Маратович <gr604_gavma@ttit.local>
2023-05-17 13:34:17 +0700

    Добавление главной функции Main() и списка городов

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using programm;
+using System.Globalization;
+
+
+Towns towns = new Towns("town_names.txt");
+
+var town_name = Searcher.Search(towns, "томск");
+
+Console.WriteLine(town_name);
+
+string? town = Computer.FindByFirstLetter(towns, 'А');
+
+while (towns.UnUsedNames.Count != 0)
+{
+    if (town == null)
+    {
+        Console.WriteLine("Компьютер больше не нашёл городов! Вы выиграли!");
+        return;
+    }
+    Console.WriteLine($"Город {town}.");
+    Console.Write($"Вам на букву {Searcher.SearchLastPermissibleLetter(town)}: ");
+    town = null;
+    while ((town = Searcher.Search(towns, Console.ReadLine() ?? "")) == null)
+    {
+        Console.WriteLine($"Такгого орода не существует!");
+    }
+    town = Computer.FindByFirstLetter(towns, Searcher.SearchLastPermissibleLetter(town));
+}
+
+
